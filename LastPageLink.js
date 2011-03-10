5c0b37aa07b0ddb01a26f8b4122ab639398e2dfb
Michael Chui <saraid216@gmail.com>
2011-03-09 19:30:29 -0800

    lastpagelink

---
+++ LastPageLink.js
@@ -0,0 +1,19 @@
+(function(){
+var footers = document.getElementsByClassName("entry-footer-info");
+for (var i = 0; i < footers.length; i++)
+{
+  var comments = footers[i].getElementsByClassName("entry-comments")[0];
+  var numComments = parseInt(comments.textContent.match(/\d+/)[0]);
+  var lastPage = Math.ceil(numComments / 50);
+  var sep = document.createElement("span");
+  YAHOO.util.Dom.addClass(sep, "seperator");
+  sep.innerHTML = " | ";
+  var href = comments.href;
+  var lastPageLink = document.createElement("a");
+  lastPageLink.setAttribute("href", href.replace(/.html(?:#comments)/, "/comments/page/"+lastPage+"#comments"));
+  lastPageLink.innerHTML = "Last Page";
+
+  YAHOO.util.Dom.insertAfter(sep, comments);
+  YAHOO.util.Dom.insertAfter(lastPageLink, sep);
+}
+})();
